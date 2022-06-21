cbcba42132abf4da40e5522693a66356b359fd0d
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2022-06-21 21:33:20 +0300

    master

--- configs/urls.py
+++ configs/urls.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from first.views import FirstView, SecondView, ThirdView, UsersView,UserRetrieveView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('first', FirstView.as_view()),
-    path('second', SecondView.as_view()),
-    path('third/<str:name>/<int:age>', ThirdView.as_view()),
-    path('users', UsersView.as_view()),
-    path('users/<int:id>', UserRetrieveView.as_view())
 ]
