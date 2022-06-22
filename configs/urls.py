6ad8ea73faf95497b00e82e84310dc0c1b10cb75
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2022-06-22 20:58:05 +0300

    hw1

--- configs/urls.py
+++ configs/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-
+from users.views import UserListCreateView, UserRetrieveUpdateDestroyView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    path('users', UserListCreateView.as_view()),
+    path('users/<int:pk>', UserRetrieveUpdateDestroyView.as_view())
 ]
